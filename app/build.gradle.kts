69aa52e4db347f47e1217ce3ad7bbe7f26b19991
Peter Mernik <peter@intech.si>
2024-05-23 07:07:31 +0200

    Updated to new KYC SDK 1.2.0 which improves onboarding.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = libs.versions.min.sdk.get().toInt()
         targetSdk = libs.versions.target.sdk.get().toInt()
         versionCode = 1
-        versionName = "1.1.0"
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -58,37 +58,37 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("io.github.vanpra.compose-material-dialogs:datetime:0.9.0")
-    implementation("io.michaelrocks:libphonenumber-android:8.13.28")
+    implementation("io.michaelrocks:libphonenumber-android:8.13.35")
     implementation("com.github.JamalMulla:ComposePrefs3:1.0.4")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     //implementation("androidx.preference:preference-ktx:1.2.1")
     implementation("io.github.vanpra.compose-material-dialogs:datetime:0.9.0")
-    implementation("io.michaelrocks:libphonenumber-android:8.13.17")
+    implementation("io.michaelrocks:libphonenumber-android:8.13.35")
     implementation("com.github.JamalMulla:ComposePrefs3:1.0.4")
     implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.compose.material:material-icons-extended:1.5.1")
+    implementation("androidx.compose.material:material-icons-extended:1.6.7")
 
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
     implementation(libs.cpp)
 
     //PayWings Android SDK
-    implementation("com.paywings.kyc:android-sdk:1.1.0")
+    implementation("com.paywings.kyc:android-sdk:1.2.0")
     implementation("com.paywings.oauth:android-sdk:2.0.0")
 
 
@@ -96,7 +96,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
